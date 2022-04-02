8d2c51a91fe2d32ff99c40478c5dc06a2c2d162c
DevTabibur <tobiburrohman2@gmail.com>
2022-04-02 16:57:08 +0600

    404 page done

--- src/components/NotFound/NotFound.jsx
+++ src/components/NotFound/NotFound.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ErrorImg from '../../images/404.webp';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './NotFound.css';
+import { Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const NotFound = () => {
     return (
@@ -12,10 +14,11 @@ const NotFound = () => {
                         <img className='error-img' src={ErrorImg} alt="error__img" />
                     </div>
                 </div>
-                <div className="col-md-6">
-                    <div className="error-page-info d-flex justify-content-cente align-items-center">
-                        <h2>404</h2>
-                        <h2>ERROR</h2>
+                <div className="col-md-6 d-flex justify-content-center align-items-center">
+                    <div className="error-page-info ">
+                    <h2 className='mb-5'>There is nothing you want!</h2>
+                        <Link to="/home" className='btn btn-primary w-100 mb-3'>Go Back to Home</Link>
+                        <Link to="/contact" className='btn btn-danger w-100'>Contact us</Link>
                     </div>
                 </div>
             </div>
