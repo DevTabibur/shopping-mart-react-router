6a0815c6a74d9ea7d68a93163400276026371088
DevTabibur <tobiburrohman2@gmail.com>
2022-04-08 02:07:06 +0600

    updated the card hover design

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
-// import './About.css';
+import './About.css';
 
 
 const About = () => {
     return (
         <>
         <div className="wrapper">
-                <div className="card">
-                    <img src="https://i.ibb.co/XsxvXSJ/t-shirt-3.webp" alt="" />
+                <div className="card-box">
+                    <img className='card-img' src="https://i.ibb.co/XsxvXSJ/t-shirt-3.webp" alt="" />
                     <div className="info">
                     <Button className='mb-5 mt-0'>WishCart</Button>
                         <Button className='d-block w-75 my-3 mx-auto info-btn'>Quick View</Button>
