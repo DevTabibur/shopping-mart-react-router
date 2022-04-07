2081b42b564253c7708e219ae8ec34d41cf40ca6
DevTabibur <tobiburrohman2@gmail.com>
2022-04-07 23:17:05 +0600

    latest product is done

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -6,55 +6,55 @@ import Counter from "../Counter/Counter";
 import { MyContext } from "../../App";
 import SwiperHome from "../SwiperHome/SwiperHome";
 import Cart from "../Cart/Cart";
-import ShowBlogHome from '../Blog/ShowBlogHome';
+import ShowBlogHome from "../Blog/ShowBlogHome";
+import { Button, ButtonGroup, Card } from "react-bootstrap";
 
 const Home = () => {
-  const [products, setProducts, blogs, setBlogs,  kids, setKids] = useContext(MyContext);
-  console.log(products.length);
+  const [products, setProducts, blogs, setBlogs, kids, setKids] =
+    useContext(MyContext);
+  const sliceProducts = products.slice(0, 8);
+
   return (
     <>
       {/* <CarouselCard />
       <Counter />
       <SwiperHome /> */}
-      <ShowProductHome
-        key={products.id}
-        products={products}
-      />
-    </>
-  );
-};
+      <div className="container  pb-5">
+        <div className="row">
+          <div className="section-title d-flex justify-content-center my-4">
+            <h2>Latest Products</h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus
+              dolor quibusdam veritatis nihil possimus soluta aliquid modi omnis
+              vel quis.
+            </p>
+          </div>
 
-
-const ShowProductHome = ({products}) => {
-  const sliceProducts = products.slice(0, 8)
-  console.log(sliceProducts);
-  return (
-    <>
-    <div className="container py-5">
-      <div className="row">
-      <div className="section-title d-flex justify-content-center my-4">
-        <h2>Latest Products</h2>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus dolor quibusdam veritatis nihil possimus soluta aliquid modi omnis vel quis.</p>
-      </div>
-        <div className="col-md-3">
-          {
-            sliceProducts.map(pd => 
-            <h2>{pd.name}</h2>
-            )
-          }
+          {sliceProducts.map((pd) => (
+            <ShowProductHome key={pd.id} pd={pd} />
+          ))}
+          <Button className="my-5 w-50 mx-auto" variant="success">Load More</Button>
         </div>
-        <div className="col-md-3"></div>
-        <div className="col-md-3"></div>
-        <div className="col-md-3"></div>
       </div>
-    </div>
-      
     </>
   );
 };
 
-
-
-
+const ShowProductHome = ({ pd }) => {
+  return (
+    <div className="col-md-3 gy-4 d-flex justify-content-center">
+      <Card style={{width: "16rem"}}>
+        <Card.Img variant="top" src={pd.image} />
+        <Card.Body>
+          <Card.Title>{pd.name}</Card.Title>
+          <Card.Text>
+            price: ${pd.price}
+          </Card.Text>
+          <Button className="w-100" variant="success">SHOW</Button>
+        </Card.Body>
+      </Card>
+    </div>
+  );
+};
 
 export default Home;
